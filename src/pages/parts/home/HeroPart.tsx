b2ecda953806ad536694c725b89938da0da2dc08
Pas <74743263+Pasithea0@users.noreply.github.com>
2025-02-22 13:16:30 -0700

    init beta

--- src/pages/parts/home/HeroPart.tsx
+++ src/pages/parts/home/HeroPart.tsx
@@ -6,6 +6,7 @@ import { SearchBarInput } from "@/components/form/SearchBar";
 import { ThinContainer } from "@/components/layout/ThinContainer";
 import { useSlashFocus } from "@/components/player/hooks/useSlashFocus";
 import { HeroTitle } from "@/components/text/HeroTitle";
+import { Heading2 } from "@/components/utils/Text";
 import { useIsTV } from "@/hooks/useIsTv";
 import { useRandomTranslation } from "@/hooks/useRandomTranslation";
 import { useSearchQuery } from "@/hooks/useSearchQuery";
@@ -77,9 +78,15 @@ export function HeroPart({ setIsSticky, searchParams }: HeroPartProps) {
     <ThinContainer>
       <div className="mt-44 space-y-16 text-center">
         <div className="relative z-10 mb-16">
-          {isTV && search.length > 0 ? null : (
-            <HeroTitle className="mx-auto max-w-md">{title}</HeroTitle>
-          )}
+          <HeroTitle>P-Stream Beta</HeroTitle>
+          <p className="text-white mt-4">
+            This is the experimental page for P-Stream. If you do not wish to
+            test features please go to the{" "}
+            <a href="https://pstream.org" className="text-type-link">
+              {" "}
+              main page.
+            </a>
+          </p>
         </div>
         <div className="relative h-20 z-30">
           <Sticky
